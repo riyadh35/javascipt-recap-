3550207b4ff465d1e51f78228e2cbad795a4687b
unknown <mdriyadhossain356@gmail.com>
2024-02-10 01:03:13 +0600

    empty string

--- RecapJavaScriptES6.js
+++ RecapJavaScriptES6.js
@@ -1,4 +1,8 @@
-var name1 = "riyad";
-var name2 = "hossain";
-var fullName = name1 + " " + name2;
-console.log(fullName);
+//  --------javascript string type and empty string --------------------
+
+// var name1 = "riyad";
+// var name2 = "hossain";
+// var fullName = name1 + " " + name2;
+// console.log(fullName);
+
+// console.log(typeof fullName);
