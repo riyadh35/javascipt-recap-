369e9c8214fad91f9dd9424f9065cfc8f8c595e5
unknown <mdriyadhossain356@gmail.com>
2024-02-13 02:22:55 +0600

    simple arrow function

---
+++ arrow-function.js
@@ -0,0 +1,13 @@
+// normal function declarations
+
+function add1(num1, num2) {
+  return num1 + num2;
+}
+const sum = add1(12, 15);
+console.log(sum);
+
+// Arrow Functions
+
+const addd2 = (num1, num2) => num1 + num2;
+const sum2 = addd2(12, 16);
+console.log(sum2);
