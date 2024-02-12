2ac5fcaafaaafe8502f8d72eeac20ce9b7555b8a
unknown <mdriyadhossain356@gmail.com>
2024-02-13 02:47:25 +0600

    what is spread operator
    :
    :

--- spread.js
+++ spread.js
@@ -1,3 +1,8 @@
+// spread operator????
+//=>The spread (...) syntax allows an iterable, such as an array or string, to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected. In an object literal, the spread syntax enumerates the properties of an object and adds the key-value pairs to the object being created.
+
+//Spread syntax looks exactly like rest syntax. In a way, spread syntax is the opposite of rest syntax. Spread syntax "expands" an array into its elements, while rest syntax collects multiple elements and "condenses" them into a single element. See rest parameters and rest property.
+
 const numbers = [12, 123, 133, 44, 55];
 // console.log(numbers);
 
