2c805dd4d24fd2a8219a95cade24de985b6b7325
unknown <mdriyadhossain356@gmail.com>
2024-02-13 02:45:07 +0600

    add spread operator

---
+++ spread.js
@@ -0,0 +1,13 @@
+const numbers = [12, 123, 133, 44, 55];
+// console.log(numbers);
+
+// for get element from arrow use Spread Eleement
+
+// console.log(...numbers);
+
+const numebr2 = [...numbers, 55];
+
+numbers.push(65);
+
+console.log(numbers);
+console.log(numebr2);
