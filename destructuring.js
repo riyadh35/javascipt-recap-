4544a4b1886a2f67ee75f8c1f9bd8957ed08b8b1
unknown <mdriyadhossain356@gmail.com>
2024-02-23 21:01:33 +0600

    destructuring in javascript

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+// destructuring in javascript
+
+const phone = {
+  id: 1,
+  name: "Iphone 14",
+  price: 50000,
+  ram: "8gb",
+  memory: "256gb",
+  company: "apples",
+};
+
+const { id, name, price, ram, memory, company } = phone;
