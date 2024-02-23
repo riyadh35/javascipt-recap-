2ab915d3c3880acd7af7ae4e3a42d0f925254ccc
unknown <mdriyadhossain356@gmail.com>
2024-02-23 21:38:13 +0600

    destructing in nested object

--- optional-chaining.js
+++ optional-chaining.js
@@ -3,3 +3,28 @@
 const myProperty = { laptop: 1, mobile: 1, bluetooth: 2, monitor: "hp" };
 
 const { monitor, laptop } = myProperty;
+
+// kono object ba array ke destructing korte hoile tar object ba array onojayi korte hove.
+
+// nested object with optional chaining
+
+const company = {
+  name: "Company",
+  category: {
+    mobile: "mobile app",
+    web: {
+      tech: {
+        framework: "tailwindcss, bootstrap",
+        library: "react.js",
+        backend: "node.js",
+      },
+      service: "web development",
+    },
+  },
+};
+
+// destructing
+
+console.log(company.category.web.service);
+
+//
