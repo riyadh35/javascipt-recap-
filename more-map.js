343d08310bd970d4c9d03893097c7994b3ee35d5
unknown <mdriyadhossain356@gmail.com>
2024-02-25 01:36:37 +0600

    added Arrow of object do map

--- more-map.js
+++ more-map.js
@@ -1,3 +1,19 @@
 const friends = ["Katrina kaif", "katrina", "deepika", "salman", "karina"];
 const flength = friends.map((friend) => friend.length);
-console.log(flength);
+// console.log(flength);
+
+// Array of objects
+
+const products = [
+  { name: "water bottle", price: 50, color: "yellow" },
+  { name: "mobile", price: "14000", color: "green" },
+  { name: "laptop", price: "67000", color: "silver" },
+];
+
+const productNames = products.map((product) => product.name);
+const productPrice = products.map((product) => product.price);
+const productColor = products.map((product) => product.color);
+
+// console.log(productNames);
+// console.log(productPrice);
+console.log(productColor);
